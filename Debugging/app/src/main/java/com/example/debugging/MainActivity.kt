e06744f905548579f626ab7dd8c258b38032dc61
anubhavprabhakar <anu.devjob@gmail.com>
2022-08-28 02:10:14 +0530

    logging
    - ERROR Logs

--- Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    fun logging() {
+        Log.e(TAG, "Hello, world!")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
     }
 }
