b290855716008f3e6f024c07806f69c3ad08ac91
anubhavprabhakar <anu.devjob@gmail.com>
2022-08-28 11:42:23 +0530

    privatised logging() and division()

--- Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -8,7 +8,7 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    fun division() {
+    private fun division() {
         val numerator = 60
         var denominator = 4
         repeat(5) {
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun logging() {
+    private fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
